a2b6980960c4aec2112179ce2bc2211d053985d2
NoManColonies <katsunari0908@gmail.com>
2021-09-01 16:19:07 +0700

    Pre-rendered grid into background image

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -62,6 +62,7 @@ struct ResponseCurveComponent : juce::Component,
   void timerCallback() override;
 
   void paint(juce::Graphics &g) override;
+  void resized() override;
 
 private:
   SimpleEQAudioProcessor &audioProcessor;
@@ -71,6 +72,12 @@ private:
   MonoChain monoChain;
 
   void updateChain();
+
+  juce::Image background;
+
+  juce::Rectangle<int> getRenderArea();
+
+  juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
