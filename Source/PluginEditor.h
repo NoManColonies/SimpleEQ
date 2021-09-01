e9e7c99b3ec800dbfe0b57cdee9f613b819fe312
NoManColonies <katsunari0908@gmail.com>
2021-09-01 11:39:28 +0700

    Created response curve component

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -18,19 +18,11 @@ struct CustomRotarySlider : juce::Slider {
                      TextEntryBoxPosition::NoTextBox) {}
 };
 
-//==============================================================================
-/**
- */
-class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor,
-                                     juce::AudioProcessorParameter::Listener,
-                                     juce::Timer {
-public:
-  SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor &);
-  ~SimpleEQAudioProcessorEditor() override;
-
-  //==============================================================================
-  void paint(juce::Graphics &) override;
-  void resized() override;
+struct ResponseCurveComponent : juce::Component,
+                                juce::AudioProcessorParameter::Listener,
+                                juce::Timer {
+  ResponseCurveComponent(SimpleEQAudioProcessor &);
+  ~ResponseCurveComponent();
 
   void parameterValueChanged(int parameterIndex, float newValue) override;
 
@@ -39,16 +31,38 @@ public:
 
   void timerCallback() override;
 
+  void paint(juce::Graphics &g) override;
+
+private:
+  SimpleEQAudioProcessor &audioProcessor;
+
+  juce::Atomic<bool> parameterChanged{false};
+
+  MonoChain monoChain;
+};
+
+//==============================================================================
+/**
+ */
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor {
+public:
+  SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor &);
+  ~SimpleEQAudioProcessorEditor() override;
+
+  //==============================================================================
+  void paint(juce::Graphics &) override;
+  void resized() override;
+
 private:
   // This reference is provided as a quick way for your editor to
   // access the processor object that created it.
   SimpleEQAudioProcessor &audioProcessor;
 
-  juce::Atomic<bool> parameterChanged{false};
-
   CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider,
       lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider,
       highCutSlopeSlider;
+
+  ResponseCurveComponent responseCurveComponent;
 
   using APVTS = juce::AudioProcessorValueTreeState;
   using Attachment = APVTS::SliderAttachment;
@@ -59,8 +73,6 @@ private:
       highCutSlopeSliderAttachment;
 
   std::vector<juce::Component *> getComps();
-
-  MonoChain monoChain;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor)
 };
