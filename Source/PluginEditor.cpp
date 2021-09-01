0a0c1c0326abe54ec8b736e2fe8c3893910fd7fe
NoManColonies <katsunari0908@gmail.com>
2021-09-01 15:02:48 +0700

    Display param value

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -25,27 +25,41 @@ void LookAndFeel::drawRotarySlider(juce::Graphics &g, int x, int y, int width,
   g.setColour(Colour(255u, 154u, 1u));
   g.drawEllipse(bounds, 1.f);
 
-  auto center = bounds.getCentre();
+  if (auto *rswl = dynamic_cast<RotarySliderWithLabels *>(&slider)) {
+    auto center = bounds.getCentre();
 
-  Path p;
+    Path p;
 
-  Rectangle<float> r;
-  r.setLeft(center.getX() - 2);
-  r.setRight(center.getX() + 2);
-  r.setTop(bounds.getY());
-  r.setBottom(center.getY());
+    Rectangle<float> r;
+    r.setLeft(center.getX() - 2);
+    r.setRight(center.getX() + 2);
+    r.setTop(bounds.getY());
+    r.setBottom(center.getY() - rswl->getTextHeight());
 
-  p.addRectangle(r);
+    p.addRoundedRectangle(r, 2.f);
+    jassert(rotaryStartAngle < rotaryEndAngle);
 
-  jassert(rotaryStartAngle < rotaryEndAngle);
+    auto sliderAngRad =
+        jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
 
-  auto sliderAngRad =
-      jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
+    p.applyTransform(
+        AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
 
-  p.applyTransform(
-      AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+    g.fillPath(p);
 
-  g.fillPath(p);
+    g.setFont(rswl->getTextHeight());
+    auto text = rswl->getDisplayString();
+    auto strWidth = g.getCurrentFont().getStringWidth(text);
+
+    r.setSize(strWidth + 4, rswl->getTextHeight() + 2);
+    r.setCentre(bounds.getCentre());
+
+    g.setColour(Colours::black);
+    g.fillRect(r);
+
+    g.setColour(Colours::white);
+    g.drawFittedText(text, r.toNearestInt(), Justification::centred, 1);
+  }
 }
 
 //==============================================================================
@@ -84,6 +98,10 @@ juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const {
   r.setY(2);
 
   return r;
+}
+
+juce::String RotarySliderWithLabels::getDisplayString() const {
+  return juce::String(getValue());
 }
 
 //==============================================================================
